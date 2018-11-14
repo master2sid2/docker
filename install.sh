0f688edd263289054bd755e54b9fc24aece79590
Vitali Khutornyy <master2sid2@i.ua>
2018-11-14 20:34:40 +0000

    docker pull mysql image

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 docker network create wordpress
 docker volume create wordpress
 docker volume create mysql_db
+docker pull centos/mysql-56-centos7:latest
 docker build --rm -f nginx/Dockerfile --tag alpine:nginx nginx/.
 docker build --rm -f php/Dockerfile --tag alpine:php-fpm php/.

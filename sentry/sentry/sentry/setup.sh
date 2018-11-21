acc57ff4445f818f2c6da31a0b08cd300ed44836
Vitaliy <master2sid2@i.ua>
2018-11-21 15:20:27 +0200

    Add --pasword for user create stage

--- sentry/sentry/sentry/setup.sh
+++ sentry/sentry/sentry/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sentry upgrade --noinput
-sentry createuser --no-input --email test@test.com --superuser --no-password
+sentry createuser --no-input --email test@test.com --password 12345678 --superuser
 $1 $2 $3

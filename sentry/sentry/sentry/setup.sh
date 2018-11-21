ce2a135345d87d5d49a4dc5c6161805e328aac85
Vitaliy <master2sid2@i.ua>
2018-11-21 16:26:45 +0200

    Fix some fackup

--- sentry/sentry/sentry/setup.sh
+++ sentry/sentry/sentry/setup.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-sentry upgrade --noinput
-sentry createuser --no-input --email test@test.com --password 12345678 --superuser
+/usr/local/bin/sentry upgrade --noinput && /usr/local/bin/sentry createuser --email test@test.com --password 12345678 --superuser --no-input
 $1 $2 $3
